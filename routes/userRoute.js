8ba797607e9253010d69cbd51274ed2e8d3d46fa
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-08 13:48:28 +0530

    Refactor user route to include Google login functionality and improve code formatting

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser, loginUser } from "./controllers/usercontroller.js";
-const userRouter=express.Router();
+import { createUser, loginUser, loginWithGoogle } from "./controllers/usercontroller.js";
 
-userRouter.post("/register",createUser)
-userRouter.post("/login",loginUser)
+const userRouter = express.Router();
+
+userRouter.post("/register", createUser);
+userRouter.post("/login", loginUser);
 userRouter.post("/login/google", loginWithGoogle);
 
 export default userRouter;
