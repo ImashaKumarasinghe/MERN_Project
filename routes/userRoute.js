50f080842936a9d2d4b5f81e581e27d73ffc1029
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-10 13:07:59 +0530

    Add getUser route with authentication to userRouter

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { createUser, loginUser, loginWithGoogle, sendOTP, resetPassword } 
+import { createUser, loginUser, loginWithGoogle, sendOTP, resetPassword, getUser } 
 from "./controllers/usercontroller.js";
+import { auth } from "../middleware/auth.js";
 
 const userRouter = express.Router();
 
@@ -8,8 +9,9 @@ userRouter.post("/register", createUser);
 userRouter.post("/login", loginUser);
 userRouter.post("/login/google", loginWithGoogle);
 userRouter.post("/reset-password", resetPassword);
-
 userRouter.post("/send-otp", sendOTP);
+userRouter.get("/", auth, getUser);
+
 
 
 export default userRouter;
