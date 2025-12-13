e686f6eab7966c291097183a76734c08cd53aaa3
ImashaKumarasinghe <167958617+ImashaKumarasinghe@users.noreply.github.com>
2025-12-13 20:49:13 +0530

    Add route to retrieve all users with authentication

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,8 @@ import express from "express";
 import { createUser, loginUser, loginWithGoogle, sendOTP, resetPassword, getUser } 
 from "./controllers/usercontroller.js";
 import { auth } from "../middleware/auth.js";
+import { getAllUsers } from "./controllers/usercontroller.js";
+
 
 const userRouter = express.Router();
 
@@ -11,6 +13,8 @@ userRouter.post("/login/google", loginWithGoogle);
 userRouter.post("/reset-password", resetPassword);
 userRouter.post("/send-otp", sendOTP);
 userRouter.get("/", auth, getUser);
+userRouter.get("/all", auth, getAllUsers); // GET /api/users/all
+
 
 
 
